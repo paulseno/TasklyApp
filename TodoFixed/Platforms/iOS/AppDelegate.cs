9516c99f6b0e9cb457c4c7a7965897d3737f5057
paulseno <paulseno550@gmail.com>
2026-03-17 08:42:41 +0800

    Final app design

---
+++ TodoFixed/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TodoFixed;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
